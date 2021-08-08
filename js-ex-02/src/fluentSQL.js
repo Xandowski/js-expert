f43b5fe76f2bdc4526b6cb13ed1931d652ba2755
Xandowski <xandy-moraes@hotmail.com>
2021-08-08 20:17:19 -0300

    semana js expert 02

---
+++ js-ex-02/src/fluentSQL.js
@@ -0,0 +1,14 @@
+export default class FluentSQLBuilder {
+  #database = []
+  constructor({ database }) {
+    this.#database = database
+  }
+
+  static for(database) {
+    return new FluentSQLBuilder({ database })
+  }
+
+  build() {
+    const results = []
+  }
+}
